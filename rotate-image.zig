9b72c4992c4413a827393771159060d3a45f0067
Jessy Pouliot <jessypouliot98@gmail.com>
2024-05-30 00:40:45 -0400

    WIP rotate image generics

--- rotate-image.zig
+++ rotate-image.zig
@@ -1,12 +1,11 @@
 const std = @import("std");
 const testing = std.testing;
 
-fn Matrix(size: comptime_int, T: type) type {
+fn Matrix(comptime size: usize, T: type) type {
     return [size][size]T;
 }
 
-// TODO Make Matrix with generic size... how ?
-// fn rotate(matrix: *Matrix(usize)) void {
+// TODO Make size & T Generic instead of hardcoding `3` & `u8`
 fn rotate(matrix: *Matrix(3, u8)) void {
     for (0..matrix.len) |i| {
         var j: u8 = 0;
@@ -18,7 +17,7 @@ fn rotate(matrix: *Matrix(3, u8)) void {
     }
 }
 
-test "it rotates a matrix as expected" {
+test "it rotates a matrix of size 3 as expected" {
     var matrix_to_rotate = Matrix(3, u8){
         .{ 1, 2, 3 },
         .{ 4, 5, 6 },
@@ -31,6 +30,23 @@ test "it rotates a matrix as expected" {
             .{ 1, 4, 7 },
             .{ 2, 5, 8 },
             .{ 3, 6, 9 },
+        },
+        matrix_to_rotate,
+    );
+}
+
+// TODO FIXME rotate only supports size 3 with values of u8
+test "it rotates a matrix of size 2 as expected" {
+    var matrix_to_rotate = Matrix(2, f16){
+        .{ 0.1, 0.2 },
+        .{ 0.3, 0.4 },
+    };
+    rotate(&matrix_to_rotate);
+
+    try testing.expectEqual(
+        Matrix(2, u8){
+            .{ 0.1, 0.3 },
+            .{ 0.2, 0.4 },
         },
         matrix_to_rotate,
     );
