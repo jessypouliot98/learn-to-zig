0f215ec6cdef9a263aa4189195f1a34e3da76089
Jessy Pouliot <jessypouliot98@gmail.com>
2024-05-30 01:07:55 -0400

    rotate image improvement

--- rotate-image.zig
+++ rotate-image.zig
@@ -1,14 +1,13 @@
 const std = @import("std");
 const testing = std.testing;
 
-fn Matrix(comptime size: usize, T: type) type {
+fn Matrix(comptime size: usize, comptime T: type) type {
     return [size][size]T;
 }
 
-// TODO Make size & T Generic instead of hardcoding `3` & `u8`
-fn rotate(matrix: *Matrix(3, u8)) void {
+fn rotate(comptime size: usize, comptime T: type, matrix: *Matrix(size, T)) void {
     for (0..matrix.len) |i| {
-        var j: u8 = 0;
+        var j: usize = 0;
         while (j <= i) : (j += 1) {
             const temp = matrix[j][i];
             matrix[j][i] = matrix[i][j];
@@ -23,7 +22,7 @@ test "it rotates a matrix of size 3 as expected" {
         .{ 4, 5, 6 },
         .{ 7, 8, 9 },
     };
-    rotate(&matrix_to_rotate);
+    rotate(3, u8, &matrix_to_rotate);
 
     try testing.expectEqual(
         Matrix(3, u8){
@@ -35,16 +34,16 @@ test "it rotates a matrix of size 3 as expected" {
     );
 }
 
-// TODO FIXME rotate only supports size 3 with values of u8
 test "it rotates a matrix of size 2 as expected" {
     var matrix_to_rotate = Matrix(2, f16){
         .{ 0.1, 0.2 },
         .{ 0.3, 0.4 },
     };
-    rotate(&matrix_to_rotate);
+    // Do I really need to defined `2` and `f16` here!?
+    rotate(2, f16, &matrix_to_rotate);
 
     try testing.expectEqual(
-        Matrix(2, u8){
+        Matrix(2, f16){
             .{ 0.1, 0.3 },
             .{ 0.2, 0.4 },
         },
