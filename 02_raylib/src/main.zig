0a608c13bba5c595b71f763392aafceb05349fbf
Jessy Pouliot <jessypouliot98@gmail.com>
2024-05-30 22:39:57 -0400

    Game WIP

--- 02_raylib/src/main.zig
+++ 02_raylib/src/main.zig
@@ -8,14 +8,27 @@ const BLACK = raylib.Color{ .r = 0x0, .g = 0x0, .b = 0x0, .a = 0x0 };
 const RED = raylib.Color{ .r = 0xFF, .g = 0x0, .b = 0x0, .a = 0x0 };
 
 const Player = struct {
-    position: u32 = 0,
+    position_x: u32 = 0,
+    position_y: u32 = 0,
 
-    pub fn update(self: Player) void {
-        print("{d}\n", .{self.position});
+    pub fn update(self: *Player) void {
+        if (raylib.IsKeyDown(raylib.KEY_W)) {
+            self.position_y += 10;
+        }
+        if (raylib.IsKeyDown(raylib.KEY_S)) {
+            self.position_y -= 10;
+        }
+        if (raylib.IsKeyDown(raylib.KEY_D)) {
+            self.position_x += 10;
+        }
+        if (raylib.IsKeyDown(raylib.KEY_A)) {
+            self.position_x -= 10;
+        }
+        print("x: {d}; y: {d};\n", .{ self.position_x, self.position_y });
     }
 
     pub fn draw(self: Player) void {
-        raylib.DrawRectangle(20, @intCast(self.position), 20, 100, BLACK);
+        raylib.DrawRectangle(@intCast(self.position_x), @intCast(self.position_y), 20, 100, BLACK);
     }
 };
 
@@ -28,7 +41,8 @@ const Game = struct {
     pub fn init() Game {
         var player = Player{};
         var game = Game{ .player = &player };
-        game.player.position = game.height / 2;
+        game.player.position_x = game.width / 2;
+        game.player.position_y = game.height / 2;
         return game;
     }
 
