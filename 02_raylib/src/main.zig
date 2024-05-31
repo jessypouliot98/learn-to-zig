30c02dc20bedbe848aea81c7163b7d3b8b435508
Jessy Pouliot <jessypouliot98@gmail.com>
2024-05-30 22:18:56 -0400

    Game WIP

--- 02_raylib/src/main.zig
+++ 02_raylib/src/main.zig
@@ -10,6 +10,10 @@ const RED = raylib.Color{ .r = 0xFF, .g = 0x0, .b = 0x0, .a = 0x0 };
 const Player = struct {
     position: u32 = 0,
 
+    pub fn update(self: Player) void {
+        print("{d}\n", .{self.position});
+    }
+
     pub fn draw(self: Player) void {
         raylib.DrawRectangle(20, @intCast(self.position), 20, 100, BLACK);
     }
@@ -19,10 +23,11 @@ const Game = struct {
     width: u32 = 800,
     height: u32 = 600,
     target_fps: u8 = 30,
-    player: Player,
+    player: *Player,
 
     pub fn init() Game {
-        var game = Game{ .player = Player{} };
+        var player = Player{};
+        var game = Game{ .player = &player };
         game.player.position = game.height / 2;
         return game;
     }
@@ -31,16 +36,25 @@ const Game = struct {
         raylib.InitWindow(@intCast(self.width), @intCast(self.height), "Game");
         raylib.SetTargetFPS(@intCast(self.target_fps));
         while (!raylib.WindowShouldClose()) {
-            raylib.BeginDrawing();
-            defer raylib.EndDrawing();
-            raylib.ClearBackground(RED);
-            self.player.draw();
-            print("{}\n", .{self.player.position});
+            self.update();
+            self.draw();
         }
+    }
+
+    pub fn update(self: Game) void {
+        self.player.update();
+    }
+
+    pub fn draw(self: Game) void {
+        raylib.BeginDrawing();
+        defer raylib.EndDrawing();
+        raylib.ClearBackground(RED);
+
+        self.player.draw();
     }
 };
 
 pub fn main() !void {
-    const game = Game.init();
+    var game = Game.init();
     game.run();
 }
