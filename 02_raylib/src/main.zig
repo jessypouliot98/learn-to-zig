31ff5faf14d59268f8c8e1d4b71da89ed5ca5c20
Jessy Pouliot <jessypouliot98@gmail.com>
2024-06-02 00:20:07 -0400

    copy paste & fix pong

--- 02_raylib/src/main.zig
+++ 02_raylib/src/main.zig
@@ -1,90 +1,91 @@
-const std = @import("std");
+// adaptation to zig 0.12.0 from https://github.com/psywave-games/pong-zig/blob/master/src/main.zig
+
 const raylib = @cImport({
     @cInclude("raylib.h");
 });
-const print = std.debug.print;
-
-const BLACK = raylib.Color{ .r = 0x0, .g = 0x0, .b = 0x0, .a = 0xFF };
-const WHITE = raylib.Color{ .r = 0xFF, .g = 0xFF, .b = 0xFF, .a = 0xFF };
-const RED = raylib.Color{ .r = 0xFF, .g = 0x0, .b = 0x0, .a = 0xFF };
-
-const Player = struct {
-    pub const WIDTH = 100;
-    pub const HEIGHT = 20;
-
-    position_x: i32,
-    position_y: i32,
-
-    pub fn init(x: i32, y: i32) Player {
-        return Player{
-            .position_x = x,
-            .position_y = y,
-        };
-    }
-
-    pub fn update(self: Player) void {
-        const speed = 10;
-        if (raylib.IsKeyDown(raylib.KEY_S)) {
-            self.position_y = @min(600 - Player.HEIGHT, self.position_y + speed);
-        }
-        if (raylib.IsKeyDown(raylib.KEY_W)) {
-            self.position_y = @max(0, self.position_y - speed);
-        }
-        if (raylib.IsKeyDown(raylib.KEY_D)) {
-            self.position_x = @min(800 - Player.WIDTH, self.position_x + speed);
-        }
-        if (raylib.IsKeyDown(raylib.KEY_A)) {
-            self.position_x = @max(0, self.position_x - speed);
-        }
-    }
-
-    pub fn draw(self: Player) void {
-        raylib.DrawRectangle(@intCast(self.position_x), @intCast(self.position_y), Player.WIDTH, Player.HEIGHT, RED);
-        print("x: {d}; y: {d};\n", .{ self.position_x, self.position_y });
-    }
-};
 
 const Game = struct {
-    width: u32,
-    height: u32,
-    target_fps: u8 = 60,
-    player: Player,
+    const fps: u8 = 60;
+    const width: u16 = 800;
+    const height: u16 = 600;
+    const min_spd: f32 = 200;
+    const max_spd: f32 = 800;
 
-    pub fn init(width: u32, height: u32) Game {
-        const game = Game{
-            .width = width,
-            .height = height,
-            .player = Player.init(
-                @intCast(width / 2),
-                @intCast(height / 2),
-            ),
-        };
-        return game;
-    }
+    const Player = struct {
+        x: f32 = 0.0,
+        y: f32 = Game.height / 2,
+        width: i32 = 10,
+        height: i32 = 80,
 
-    pub fn run(self: Game) void {
-        raylib.InitWindow(@intCast(self.width), @intCast(self.height), "Game");
-        raylib.SetTargetFPS(@intCast(self.target_fps));
-        while (!raylib.WindowShouldClose()) {
-            self.update();
-            self.draw();
+        fn update(self: *Player) void {
+            const down: i2 = @intFromBool(raylib.IsKeyDown(raylib.KEY_DOWN));
+            const up: i2 = @intFromBool(raylib.IsKeyDown(raylib.KEY_UP));
+            const speed: f32 = @as(f32, @floatFromInt(down - up)) * raylib.GetFrameTime() * Game.min_spd * 3;
+            self.y = @min(@max(self.y + speed, 0), Game.height);
         }
+
+        fn draw(self: *Player) void {
+            raylib.DrawRectangle(@intFromFloat(self.x), @as(i32, @intFromFloat(self.y)) - self.height, self.width, self.height * 2, raylib.WHITE);
+        }
+    };
+
+    const Ball = struct {
+        x: f32,
+        y: f32,
+        hspeed: f32,
+        vspeed: f32,
+        size: f32 = 8.0,
+
+        fn create() Ball {
+            return Ball{ .x = Game.width / 2, .y = Game.height / 2, .hspeed = -Game.min_spd, .vspeed = 0 };
+        }
+
+        fn update(self: *Ball) void {
+            self.x += raylib.GetFrameTime() * self.hspeed;
+            self.y += raylib.GetFrameTime() * self.vspeed;
+        }
+
+        fn draw(self: Ball) void {
+            raylib.DrawCircle(@intFromFloat(self.x), @intFromFloat(self.y), self.size, raylib.WHITE);
+        }
+    };
+
+    pub fn app() void {
+        raylib.InitWindow(Game.width, Game.height, "Pong Game");
+        raylib.SetTargetFPS(Game.fps);
+
+        var player = Player{};
+        var ball = Ball.create();
+
+        var application = Game{ .player = &player, .ball = &ball };
+
+        while (!raylib.WindowShouldClose()) {
+            application.update();
+            application.draw();
+        }
+
+        raylib.CloseWindow();
     }
 
-    pub fn update(self: Game) void {
+    fn update(self: Game) void {
         self.player.update();
+        self.ball.update();
     }
 
-    pub fn draw(self: Game) void {
+    fn draw(self: Game) void {
         raylib.BeginDrawing();
-        defer raylib.EndDrawing();
 
-        raylib.ClearBackground(BLACK);
+        raylib.ClearBackground(raylib.BLACK);
         self.player.draw();
+        self.ball.draw();
+
+        raylib.EndDrawing();
     }
+
+    player: *Player,
+    ball: *Ball,
 };
 
-pub fn main() !void {
-    var game = Game.init(800, 600);
-    game.run();
+pub fn main() anyerror!void {
+    Game.app();
 }
