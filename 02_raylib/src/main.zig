fa1d204af273572407ca50d809e394514c821645
Jessy Pouliot <jessypouliot98@gmail.com>
2024-06-01 22:09:19 -0400

    this is crap

--- 02_raylib/src/main.zig
+++ 02_raylib/src/main.zig
@@ -8,52 +8,75 @@ const BLACK = raylib.Color{ .r = 0x0, .g = 0x0, .b = 0x0, .a = 0xFF };
 const WHITE = raylib.Color{ .r = 0xFF, .g = 0xFF, .b = 0xFF, .a = 0xFF };
 const RED = raylib.Color{ .r = 0xFF, .g = 0x0, .b = 0x0, .a = 0xFF };
 
-const Ball = struct {};
+const Position = struct {
+    x: i32,
+    y: i32,
+};
 
 const Player = struct {
     pub const WIDTH = 100;
     pub const HEIGHT = 20;
-    position_x: i32 = 0,
-    position_y: i32 = 0,
+
+    position: Position = Position{
+        .x = 0,
+        .y = 0,
+    },
 
     pub fn update(self: *Player) void {
         const speed = 10;
         if (raylib.IsKeyDown(raylib.KEY_S)) {
-            self.position_y = @min(600 - Player.HEIGHT, self.position_y + speed);
+            self.position.y = @min(600 - Player.HEIGHT, self.position.y + speed);
         }
         if (raylib.IsKeyDown(raylib.KEY_W)) {
-            self.position_y = @max(0, self.position_y - speed);
+            self.position.y = @max(0, self.position.y - speed);
         }
         if (raylib.IsKeyDown(raylib.KEY_D)) {
-            self.position_x = @min(800 - Player.WIDTH, self.position_x + speed);
+            self.position.x = @min(800 - Player.WIDTH, self.position.x + speed);
         }
         if (raylib.IsKeyDown(raylib.KEY_A)) {
-            self.position_x = @max(0, self.position_x - speed);
+            self.position.x = @max(0, self.position.x - speed);
         }
     }
 
     pub fn draw(self: Player) void {
-        raylib.DrawRectangle(@intCast(self.position_x), @intCast(self.position_y), Player.WIDTH, Player.HEIGHT, RED);
-        print("x: {d}; y: {d};\n", .{ self.position_x, self.position_y });
+        raylib.DrawRectangle(@intCast(self.position.x), @intCast(self.position.y), Player.WIDTH, Player.HEIGHT, RED);
+        print("x: {d}; y: {d};\n", .{ self.position.x, self.position.y });
     }
 };
 
+const Brick = struct {
+    pub const WIDTH = 100;
+    pub const HEIGHT = 20;
+
+    position: Position = Position{
+        .x = 0,
+        .y = 0,
+    },
+};
+
 const Game = struct {
-    width: u32 = 800,
-    height: u32 = 600,
+    width: u32,
+    height: u32,
     target_fps: u8 = 60,
     player: *Player,
-    ball: *Ball,
+    brick: *Brick,
 
-    pub fn init() Game {
-        var player = Player{};
-        var ball = Ball{};
-        const game = Game{
-            .player = &player,
-            .ball = &ball,
+    pub fn init(width: u32, height: u32) Game {
+        var player = Player{
+            .position = Position{
+                .x = @intCast(width / 2),
+                .y = @intCast(height / 2),
+            },
         };
-        game.player.*.position_x = @intCast(game.width / 2);
-        game.player.*.position_y = @intCast(game.height / 2);
+
+        var brick = Brick{};
+
+        const game = Game{
+            .width = width,
+            .height = height,
+            .player = &player,
+            .brick = &brick,
+        };
         return game;
     }
 
@@ -80,6 +103,6 @@ const Game = struct {
 };
 
 pub fn main() !void {
-    var game = Game.init();
+    var game = Game.init(800, 600);
     game.run();
 }
