2481a62f28470ba6b919b7d84f8d3332ff2ea0c8
Jessy Pouliot <jessypouliot98@gmail.com>
2024-06-13 23:20:19 -0400

    challenge twitter completed

--- 08_challenge_twitter/challenge.zig
+++ 08_challenge_twitter/challenge.zig
@@ -33,42 +33,44 @@ const Twitter = struct {
         try self.tweets_user_map.put(tweet_id, user_id);
     }
 
-    pub fn getNewsFeed(self: *Twitter, user_id: u32) !std.ArrayList(u32) {
-        var feed = std.ArrayList(u32).init(self.allocator);
+    pub fn getNewsFeed(self: *Twitter, user_id: u32) ![]u32 {
+        var feed_list = std.ArrayList(u32).init(self.allocator);
 
         const followees = self.follower_followees_map.get(user_id);
 
         var tweet_user_iterator = self.tweets_user_map.iterator();
         while (tweet_user_iterator.next()) |tweet_user| {
-            if (tweet_user.value_ptr.* == user_id) {
-                try feed.append(tweet_user.key_ptr.*);
-            } else if (followees != null and followees.?.contains(tweet_user.value_ptr.*)) {
-                // Debug
-                try feed.append(tweet_user.key_ptr.*);
+            if ((tweet_user.value_ptr.* == user_id) or
+                (followees != null and followees.?.contains(tweet_user.value_ptr.*)))
+            {
+                try feed_list.append(tweet_user.key_ptr.*);
             }
         }
 
-        return feed;
+        if (feed_list.items.len <= 1) {
+            return feed_list.items;
+        }
+
+        const ordered_feed = try self.allocator.alloc(u32, feed_list.items.len);
+        std.debug.print("hello\n", .{});
+
+        for (feed_list.items, 1..) |item, i| {
+            ordered_feed[feed_list.items.len - i] = item;
+        }
+        return ordered_feed;
     }
 
     pub fn follow(self: *Twitter, follower_id: u32, followee_id: u32) !void {
-        var followees_map: std.AutoHashMap(u32, void) = undefined;
-        if (self.follower_followees_map.get(follower_id)) |fm| {
-            followees_map = fm;
-        } else {
-            followees_map = std.AutoHashMap(u32, void).init(self.allocator);
-            try self.follower_followees_map.put(follower_id, followees_map);
+        const followee_set = try self.follower_followees_map.getOrPut(follower_id);
+        if (!followee_set.found_existing) {
+            followee_set.value_ptr.* = std.AutoHashMap(u32, void).init(self.allocator);
         }
-
-        try followees_map.put(followee_id, {});
+        try followee_set.value_ptr.*.put(followee_id, {});
     }
 
     pub fn unfollow(self: *Twitter, follower_id: u32, followee_id: u32) void {
-        if (self.follower_followees_map.get(follower_id)) |*followees_map| {
-            // Debug
-            // _ = followees_map.remove(followee_id);
-            _ = followees_map;
-            _ = followee_id;
+        if (self.follower_followees_map.getEntry(follower_id)) |entry| {
+            _ = entry.value_ptr.*.remove(followee_id);
         }
     }
 };
@@ -83,35 +85,32 @@ test "challenge" {
 
     try twitter.postTweet(1, 5);
     {
-        var result = try twitter.getNewsFeed(1);
-        defer result.deinit();
+        const result = try twitter.getNewsFeed(1);
         try std.testing.expectEqualSlices(
             u32,
             &.{5},
-            result.items,
+            result,
         );
     }
 
     try twitter.follow(1, 2);
     try twitter.postTweet(2, 6);
     {
-        var result = try twitter.getNewsFeed(1);
-        defer result.deinit();
+        const result = try twitter.getNewsFeed(1);
         try std.testing.expectEqualSlices(
             u32,
             &.{ 6, 5 },
-            result.items,
+            result,
         );
     }
 
     twitter.unfollow(1, 2);
     {
-        var result = try twitter.getNewsFeed(1);
-        defer result.deinit();
+        const result = try twitter.getNewsFeed(1);
         try std.testing.expectEqualSlices(
             u32,
             &.{5},
-            result.items,
+            result,
         );
     }
 }
